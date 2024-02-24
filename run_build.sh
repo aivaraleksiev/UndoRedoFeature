099a269ed3f2c3bafacf8302dff076502d452c78
Aivar Aleksiev <aivar.aleksiev@gmail.com>
2024-02-24 20:38:27 +0200

    Update run_build.sh

--- run_build.sh
+++ run_build.sh
@@ -9,7 +9,6 @@
 conan install . -s build_type=Release -s compiler.cppstd=gnu23 --output-folder=build-x64/conan --build=missing
 conan install . -s build_type=Debug -s compiler.cppstd=gnu23 --output-folder=build-x64/conan --build=missing
 
-pwd
 pushd ./build-x64
 
 cmake -S .. -B . -D CMAKE_TOOLCHAIN_FILE=./conan/conan_toolchain.cmake -D CMAKE_BUILD_TYPE=Debug
