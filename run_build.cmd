b30244ab216d716f5a87dfa8a3349afd1aedbdff
Aivar Aleksiev <aivar.aleksiev@gmail.com>
2024-03-06 01:07:22 +0200

    Update backslashes to windows style

--- run_build.cmd
+++ run_build.cmd
@@ -1,17 +1,17 @@
 @REM From source folder run command to install dependent packages
 @REM Install both release and debug package version, so we can build in Debug/Release
 
-conan install . -s build_type=Release -s compiler.cppstd=23 --output-folder=build-x64/conan --build=missing
-conan install . -s build_type=Debug -s compiler.cppstd=23 --output-folder=build-x64/conan --build=missing
+conan install . -s build_type=Release -s compiler.cppstd=23 --output-folder=build-x64\conan --build=missing
+conan install . -s build_type=Debug -s compiler.cppstd=23 --output-folder=build-x64\conan --build=missing
 
 pushd .\build-x64\
 
 @REM Configure Visual Studio project
-cmake -S .. -B . -G "Visual Studio 17 2022" -D CMAKE_TOOLCHAIN_FILE=./conan/conan_toolchain.cmake -D CMAKE_BUILD_TYPE=Release
+cmake -S .. -B . -G "Visual Studio 17 2022" -D CMAKE_TOOLCHAIN_FILE=.\conan\conan_toolchain.cmake -D CMAKE_BUILD_TYPE=Release
 
 popd
 REM Start Compilation
-cmake --build ./build-x64 --config Release
+cmake --build .\build-x64 --config Release
 
 # Run tests
 # ctest -C Release
